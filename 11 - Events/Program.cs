5a08ab332e2e87136909f4f3dd0e9646b9924f68
byniov1 <karol.nowak0490@protonmail.com>
2025-05-19 10:12:40 +0200

    feat: small intro to generics

--- 11 - Events/Program.cs
+++ 11 - Events/Program.cs
@@ -65,7 +65,37 @@ internal class Program
 
         static void Main(string[] args)
         {
-           
+            int[] intArray = { 1, 2, 3, 4, 5, };
+            string[] stringArray = { "One", "Two", "Three", "Four" };
+            PrintArray(intArray);
+            PrintArray(stringArray);
+            Console.ReadKey();
+
+        }
+
+        public static void PrintIntArray(int[] array)
+        {
+            foreach (int item in array)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        public static void PrintStringArray(string[] array)
+        {
+            foreach (string item in array)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        public static void PrintArray<T>(T[] array)
+        {
+            foreach (T item in array)
+            {
+                Console.Write($"{item} ");
+            }
+            Console.WriteLine();
         }
 
     }
